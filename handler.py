619b26eb609821ae6e817e33c9abc178166f9aa5
ikhwanrnurzaman <ikhwanrnurzaman@gmail.com>
2020-07-03 11:32:48 +0700

    using message queue protocol for distribute messages

--- handler.py
+++ handler.py
@@ -4,35 +4,69 @@ from uuid import uuid4
 # Third-party libray
 from rx.core import Observer
 from aiohttp import web
-from aiopg.sa import Engine
+from aio_pika import Message, DeliveryMode
 
 
 class OrderHandler(Observer):
 
-    def __init__(self, engine: Engine):
+    def __init__(self):
         super().__init__()
-        self.dbEngine = engine
 
     def on_next(self, request: web.Request):
         loop = request['loop']
         async def asyncOrder(request):
             future = request['future']
+            body = request['body']
+            channel = request['channel']
+            queue = request['queue']
 
-            try:
-                params = await request.json()
-                clientRef = params.get('client_ref')
-                account = params.get('account')
-                action = params.get('action')
-                stock = params.get('stock')
-                price = params.get('price')
-                vol = params.get('vol')
-                orderId = str(uuid4())[:8]
+            clientRef = body['client_ref']
+            account = body['account']
+            action = body['action']
+            stock = body['stock']
+            price = body['price']
+            vol = body['vol']
+            orderId = str(uuid4())[:8]
 
-                rawSql = 'INSERT INTO orders (account, action, stock, price, vol, order_id) VALUES (%s, %s, %s, %s, %s, %s)'
-                async with self.dbEngine.acquire() as dbConn:
-                    await dbConn.execute(rawSql, (account, action, stock, price, vol, orderId))
-                future.set_result(web.json_response({'status':'OK', 'order_id': orderId, 'client_ref':clientRef}))
-            except Exception as e:
-                future.set_result(web.json_response({'status':'FAIL', 'order_id': None, 'client_ref': None}))
+            msg = f"{account}|{action}|{stock}.{price}.{vol}.{orderId}"
+
+            msg = Message(msg.encode(), delivery_mode=DeliveryMode.PERSISTENT)
+
+            await channel.default_exchange.publish(msg, routing_key=queue)
+
+            future.set_result(web.json_response({'status':'OK', 'order_id': orderId, 'client_ref':clientRef}))
 
         loop.create_task(asyncOrder(request))
+
+    def orderVerificator(self, request: web.Request) -> bool:
+        body = request['body']
+        future = request['future']
+        verified = True
+
+        paramsExpect = ('client_ref', 'account', 'action', 'stock', 'price', 'vol')
+
+        paramsMissing = list(filter(lambda i: i not in body, paramsExpect))
+
+        if len(paramsMissing) > 0:
+            body['verified'] = False
+            future.set_result(web.json_response({'status':'FAIL', 'order_id': None, 'client_ref': None, 'reason':f"Missing parameter {','.join(paramsMissing)}"},
+                                                status=422))
+            return False
+
+        if body['action'] not in ('BUY', 'SELL'):
+            verified = False
+            reason = 'Action must be BUY or SELL'
+
+        if not isinstance(body['price'], int) or not isinstance(body['vol'], int):
+            verified = False
+            reason = 'Price and volume must be integer'
+
+        elif body['price'] <= 0 or body['vol'] <= 0:
+            verified = False
+            reason = 'Price and volume must be > 0'
+
+        if not verified:
+            future.set_result(web.json_response({'status':'FAIL', 'order_id': None, 'client_ref': body['client_ref'], 'reason': reason},
+                                                status=422))
+
+        return verified
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import rx.operators as ops
 from rx.subject import Subject
 from rx.scheduler.eventloop import AsyncIOScheduler
 from aiohttp import web
-from aiopg.sa import create_engine
+from aio_pika import connect_robust
 
 # Costum library
 from handler import OrderHandler
@@ -20,14 +20,15 @@ LOG_REQUEST_HANDLER.setFormatter(LOG_REQUEST_FORMAT)
 class Webserver:
     def __init__(self):
         self.app = web.Application()
-        self.dbEngine = None
-        self.loop = asyncio.get_event_loop()
-        self.requestLogger = logging.getLogger('request')
-        self.requestLogger.addHandler(LOG_REQUEST_HANDLER)
-        self.requestLogger.setLevel(logging.INFO)
         self.loop = asyncio.get_event_loop()
         self.request = Subject() # Requests observable object
         self.subscriptions = [] # List for containing disposable request observer
+        self.rmqConn = None
+        self.channel = {}
+        self.orderHandler = OrderHandler()
+        self.requestLogger = logging.getLogger('request')
+        self.requestLogger.addHandler(LOG_REQUEST_HANDLER)
+        self.requestLogger.setLevel(logging.INFO)
 
 
     def logRequest(self, request : web.Request):
@@ -38,6 +39,15 @@ class Webserver:
         future = asyncio.Future()
         request['future'] = future
         request['loop'] = self.loop
+
+        try:
+            body = await request.json()
+            request['body'] = body
+            request['queue'] = request.path.replace('/','')
+            request['channel'] = self.channel[request.path]
+        except Exception as e:
+            return web.json_response({'status':'FAIL', 'order_id': None, 'client_ref': None, 'reason':'Failed to parse request body'}, status=400)
+
         self.request.on_next(request) # Pass the request to the observers
 
         await future
@@ -46,19 +56,28 @@ class Webserver:
         return result
 
     async def on_shutdown(self, app: web.Application):
-        self.dbEngine.close()
+        await self.rmqConn.close()
         map(lambda i: i.dispose(), self.subscriptions)
-        await self.dbEngine.wait_closed()
 
 
     async def init(self):
-        self.dbEngine = await create_engine(user='reactive', password='reaction', database='reactive', host='localhost')
+        # Establish connection to RabbitMQ
+        self.rmqConn = await connect_robust(login='ikhwanrnurzaman', password='123456')
+
+        # Establish channel for order request and declare order queue
+        self.channel['/order'] = await self.rmqConn.channel()
+        await self.channel['/order'].declare_queue('order', durable=True)
+
+        # Establish channel for account request and declare account queue
+        self.channel['/account'] = await self.rmqConn.channel()
+        await self.channel['/account'].declare_queue('account', durable=True)
 
         # Create disposable request observer for handling order request. Only request to /order will be passed to OrderHandler
         dispose = self.request.pipe(
             ops.do_action(self.logRequest),
-            ops.filter(lambda i : i.path == '/order')
-        ).subscribe(OrderHandler(self.dbEngine), scheduler=AsyncIOScheduler)
+            ops.filter(lambda i : i.path == '/order'),
+            ops.filter(self.orderHandler.orderVerificator)
+        ).subscribe(self.orderHandler, scheduler=AsyncIOScheduler)
         self.subscriptions.append(dispose)
 
         self.app.router.add_post('/order', self.dispatcher, name='order')
