4d51aba040faa95fa85178ff730601d0b1066ae4
ikhwanrnurzaman <ikhwanrnurzaman@gmail.com>
2020-07-21 10:56:41 +0700

    Update database dump

--- reactive_database.sql
+++ reactive_database.sql
@@ -38,9 +38,36 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: reactive
+--
+
+CREATE TABLE public.accounts (
+    account_no character varying NOT NULL,
+    balance bigint DEFAULT 0 NOT NULL,
+    balance_hold bigint DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.accounts OWNER TO reactive;
 
 --
 -- Name: orders; Type: TABLE; Schema: public; Owner: bahana_olt
@@ -58,6 +85,34 @@ CREATE TABLE public.orders (
 
 
 ALTER TABLE public.orders OWNER TO bahana_olt;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    username character varying NOT NULL,
+    password character varying NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: reactive
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (account_no);
+
+
+--
+-- Name: accounts unique_accounts_account_no; Type: CONSTRAINT; Schema: public; Owner: reactive
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT unique_accounts_account_no UNIQUE (account_no);
+
 
 --
 -- PostgreSQL database dump complete
